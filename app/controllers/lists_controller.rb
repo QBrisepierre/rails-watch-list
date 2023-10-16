c61423f26173de335534a322eb46639aee9ba5fb
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-10-16 14:28:24 +0200

    Design and start user add movie to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,33 @@
 class ListsController < ApplicationController
+  before_action :set_params, only: [:show]
+
   def index
     @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_params
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new]
+  end
 end
